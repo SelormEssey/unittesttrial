3c9649782a9beb6a767e7958a0bfd83a3acd5043
Hiruy Worku <hworku27@wooster.edu>
2024-09-02 12:39:44 -0400

    i added some calculus

--- unittest.py
+++ unittest.py
@@ -34,4 +34,43 @@ class TestElectricCar(unittest.TestCase):
 if __name__ == '__main__':
    unittest.main()
 
+#code added by the contributor
 
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return x / y 
+
+class Testcalc(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(-1, 1), -2)
+        self.assertEqual(calc.subtract(-1, -1), 0)
+    
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10, 5), 50)
+        self.assertEqual(calc.multiply(-1, 1), -1)
+        self.assertEqual(calc.multiply(-1, -1), 1)
+    
+    def test_divide(self):
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
+
+if __name__ == '__main__':
+    unittest.main()

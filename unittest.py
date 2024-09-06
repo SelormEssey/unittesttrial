b2b4d42ea9e4e541152aa3962cf0db0a20e611ec
Keith Young <keithy822@gmail.com>
2024-09-06 12:34:24 -0400

    added cost to the Vehicle class

--- unittest.py
+++ unittest.py
@@ -1,11 +1,15 @@
 class Vehicle:
-   def __init__(self, make, model, year):
+   def __init__(self, make, model, year, cost):
        self.make = make
        self.model = model
        self.year = year
+       self.cost = cost
   
    def vehicle_info(self):
-       return f"{self.year} {self.make} {self.model}"
+       return f"{self.year} {self.make} {self.model} {self.cost}"
+   
+   def compare_cost_year(self):
+       return f"The car cost:{self.cost} and was manufactued in :{self.year}"
 
 
 class ElectricCar(Vehicle):
@@ -17,6 +21,10 @@ class ElectricCar(Vehicle):
    def calculate_range(self):
        average_consumption = 0.2  # kWh per mile, for simplicity
        return (self.current_charge / average_consumption)
+   
+
+        
+   
 import unittest
 
 
@@ -29,6 +37,11 @@ class TestElectricCar(unittest.TestCase):
    def test_calculate_range_partial_charge(self):
        car = ElectricCar("Tesla", "Model 3", 2023, 75, 30)
        self.assertEqual(car.calculate_range(), 150)
+       
+   def test_car_cost_year(self):
+       car = Vehicle("Honda", "Civic", 2012, 10,000)
+       self.assertIs(car.cost(), 10,000)
+        
 
 
 if __name__ == '__main__':
